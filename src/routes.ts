8b7695bb9ef8cc7ba1d484dff9ad524a4991848e
Akihira77 <tik.dikawahyu123@gmail.com>
2024-05-19 13:38:28 +0700

    fix main branch

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,11 @@
 import { Application } from "express";
 import { healthRoutes } from "@chat/routes/health.route";
 import { chatRoutes } from "@chat/routes/chat.route";
+import { verifyGatewayRequest } from "@Akihira77/jobber-shared";
 
 const BASE_PATH = "/api/v1/message";
 
 export function appRoutes(app: Application): void {
     app.use("", healthRoutes());
-    app.use(BASE_PATH, chatRoutes());
+    app.use(BASE_PATH, verifyGatewayRequest, chatRoutes());
 }
